a1c117c4369128753b2254a9ed0ef42592f4c3ab
argenpine <alf.lafosse@gmail.com>
2026-04-23 22:43:17 +0930

    -its fun

--- TAG/Assets/Scripts/Obstacles/BoostPad.cs
+++ TAG/Assets/Scripts/Obstacles/BoostPad.cs
@@ -4,11 +4,27 @@ public class BoostPad : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D other)
     {
-        Debug.Log(other.name + "entered boost trigger");
+        
         if (other.gameObject.CompareTag("Player"))
         {
             PlayerMovement playerMovement = other.GetComponent<PlayerMovement>();
             playerMovement.BoostPad();
+            return;
         }
+
+        else
+        {
+            Rigidbody2D rb = other.attachedRigidbody;
+
+            if(rb != null)
+            {
+
+                Vector2 moveDirection = rb.linearVelocity.normalized;
+
+                rb.AddForce(moveDirection * 35f, ForceMode2D.Impulse);
+            }
+        }
+
+        
     }
 }
--- TAG/Assets/Scripts/Obstacles/Portal.cs
+++ TAG/Assets/Scripts/Obstacles/Portal.cs
@@ -9,12 +9,14 @@ public class Portal : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        Debug.Log(other.name + "entered portal trigger");
+
+        
+
         if (other.gameObject.CompareTag("Player"))
         {
-            PlayerMovement playerMovement = other.GetComponent<PlayerMovement>();
             int randomIndex = Random.Range(0, linkedPortals.Count);
             other.gameObject.transform.position = linkedPortals[randomIndex].position;
+            PlayerMovement playerMovement = other.GetComponent<PlayerMovement>();
             playerMovement.PlayerTeleported();
         }
     }
